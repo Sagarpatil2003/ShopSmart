509ac5e5c05c725f3a393d4f65ef10cd4f6957e1
Sagarpatil2003 <sp4765699@gmal.com>
2026-01-05 22:41:50 +0530

    api that buy all selected product in cart

--- BackEnd/routers/orderRoute.js
+++ BackEnd/routers/orderRoute.js
@@ -15,6 +15,12 @@ let OrderRouter = express.Router()
 
 OrderRouter.get('/order-history', authMiddleware(["user"]), getHistory)
 
+OrderRouter.get('/order/:orderId', authMiddleware(["user"]), getOrderById)
+
+OrderRouter.post('/buy-all-items', authMiddleware(['user']), orderAllCartItem)
+
+OrderRouter.post('/buy-selected-items', authMiddleware(['user']), orderSelectedFromCart)
+
 
 
 
