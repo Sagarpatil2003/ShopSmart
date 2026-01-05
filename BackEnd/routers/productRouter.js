a9a0c66b7359faa85a492fa4e60870567e6b2981
Sagarpatil2003 <sp4765699@gmal.com>
2026-01-05 22:32:14 +0530

    create the product CRUD api's for admin

---
+++ BackEnd/routers/productRouter.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const authMiddleware = require('../middlewares/authMiddlewares')
+const { createProduct, updateProduct, deleteProduct, updateInventory, productList, getProduct, searchProduct } = require('../controllers/productControllers')
+const ProductRouter = express.Router()
+
+// admin crud api
+ProductRouter.post('/create', authMiddleware(['admin']), createProduct)
+
+ProductRouter.put('/update-product/:id', authMiddleware(['admin']), updateProduct)
+
+ProductRouter.delete('/delete-product/:id', authMiddleware(['admin']), deleteProduct)
+
+ProductRouter.post('/update-inventory/:id', authMiddleware(['admin']), updateInventory)
+
+// User api
+ProductRouter.get('/product-list', productList)
+
+ProductRouter.get('/product/:id', getProduct)
+
+ProductRouter.get("/search", searchProduct)
+
+
+
+
+module.exports = ProductRouter
