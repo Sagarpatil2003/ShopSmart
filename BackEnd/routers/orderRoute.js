43c70be9573d0fdfa95243b9bf62db9d47cc3fb0
Sagarpatil2003 <sp4765699@gmal.com>
2026-01-05 22:36:18 +0530

    api for get user orders

---
+++ BackEnd/routers/orderRoute.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const authMiddleware = require('../middlewares/authMiddlewares')
+const {
+    orderAllCartItem,
+    orderSelectedFromCart,
+    buyNowOrder,
+    returnOrder,
+    getHistory,
+    getOrderById
+} 
+= require('../controllers/orderController')
+
+let OrderRouter = express.Router()
+
+
+OrderRouter.get('/order-history', authMiddleware(["user"]), getHistory)
+
+OrderRouter.get('/order/:orderId', authMiddleware(["user"]), getOrderById)
+
+OrderRouter.post('/buy-all-items', authMiddleware(['user']), orderAllCartItem)
+
+OrderRouter.post('/buy-selected-items', authMiddleware(['user']), orderSelectedFromCart)
+
+OrderRouter.post('/buy-now', authMiddleware(['user']), buyNowOrder)
+
+OrderRouter.post('/return-order/:orderId', authMiddleware(['user']), returnOrder)
+
+
+module.exports = OrderRouter
+
+
