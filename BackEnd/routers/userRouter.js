35bf640fc138b443ff4aa2ecaa24f4cb59377693
Sagarpatil2003 <sp4765699@gmal.com>
2026-01-05 22:20:33 +0530

    create the api for user login

--- BackEnd/routers/userRouter.js
+++ BackEnd/routers/userRouter.js
@@ -16,14 +16,6 @@ UserRoute.post('/signup', userSignUp)
 
 UserRoute.post('/login', loginUser)
 
-UserRoute.post('/forget-password', forgetPassword);
 
-UserRoute.post('/reset-password', resetPassword);
-
-UserRoute.get('/user-profile', authMiddleware(['user', 'admin']), getUser)
-
-UserRoute.put('/update-profile', authMiddleware(['user']), updateUserProfile)
-
-UserRoute.get('/history', authMiddleware(['user', 'admin']), orderHistory)
 
 module.exports = UserRoute
